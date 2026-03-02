5ffd892ec0bd4c05fb1f6d46a44b611738727cd0
epicgitaccountnumber2 <19139909@hlnsc.ac.uk>
2026-03-02 15:25:35 +0000

    more frontend

---
+++ Frontend code.py
@@ -0,0 +1,14 @@
+import flet as ft
+
+def loginPage(page: ft.Page)
+    page.title = "Secure Login System"
+    page.verticle_alignment = ft.MainAxisAlightment.center
+
+    input = ft.TextField(value="0", text_align=ft.TextAlign.LEFT, width=100)
+
+    page.add(
+        ft.Row(
+            alighment=ft.MainAxisAlighment.CENTER,
+            CONTROLS=[ft.Icons.REMOVE, on+click=]
+        )
+    )
