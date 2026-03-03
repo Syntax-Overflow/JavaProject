bc4d66fb0bc6446db7347cc1abb5c4125d2b1065
epicgitaccountnumber2 <19139909@hlnsc.ac.uk>
2026-03-03 11:41:40 +0000

    started work

--- Frontend code.py
+++ Frontend code.py
@@ -1,7 +1,20 @@
 import FreeSimpleGUI as sg
 
-layout = [[sg.Text("Input Username")]
-          [sg.Input(key="-INPUT-")]
-          
-          
-          ]
+# Layout
+layout = [[sg.Text("Input Username")],
+          [sg.Input(key='User')],
+          [sg.Text("Input Password")],
+          [sg.Input(key="Pass", password_char="*")]]
+
+# Create login window
+window = sg.Window('Login', layout)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+
+# Finish up by removing from the screen
+window.close()
